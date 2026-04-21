728b481794ff27efedc14a78cbdc9ae0b1ddff53
Prince Sharma <krrishsharma698@gmail.com>
2026-04-21 16:53:18 +0530

    First diploy

---
+++ Contact_App/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def Home(request):
+    if request.method == 'POST':
+        Name = request.POST.get('name')
+        Email = request.POST.get('email')
+
+        return render(request, 'website/profile.html', {
+            'name' : Name,
+            'email' : Email
+        })
+    return render(request, 'website/index.html')
+
+def profile(request):
+    return render(request, 'website/profile.html')
